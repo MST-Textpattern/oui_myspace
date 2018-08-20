de542e1abdb3453ac38b8519532927ad559ffaaa
Nicolas Morand <contact@nicolasmorand.com>
2018-08-20 14:41:18 +0200

    Updates the Provider class namespace

--- oui_myspace.php
+++ oui_myspace.php
@@ -31,9 +31,9 @@
 
 namespace Oui;
 
-if (class_exists('Oui\Provider')) {
+if (class_exists('Oui\Player\Provider')) {
 
-    class Myspace extends Provider
+    class Myspace extends Player\Provider
     {
         protected static $srcBase = '//media.myspace.com/';
         protected static $srcGlue = array('play/video/', '?', '&amp;');
